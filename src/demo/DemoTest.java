ac29c64592a0eea82a1a4f52dda163d580208c75
bashi <bashi@192.168.0.18>
2022-08-01 19:11:41 -0400

    Implemented US

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -4,5 +4,10 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("Test");
 	}
+	
+	//for US
+	public static void add(int a, int b) {
+		System.out.println(a-b);
+	}
 
 }
